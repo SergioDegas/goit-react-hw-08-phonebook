4e5130ef623d22e657caa47ed2b5345c29ede87c
SergioDegas <104730479+SergioDegas@users.noreply.github.com>
2023-01-23 20:18:06 +0000

    Deploying to gh-pages from @ SergioDegas/goit-react-hw-08-phonebook@58318a0a8301f5257c55eed35a4ca11db65a1dc3 🚀

---
+++ static/js/859.700240b6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[859],{4859:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var t=r(184);function o(){return(0,t.jsx)("div",{children:(0,t.jsxs)("h1",{children:["Welcome to the phone book app"," ",(0,t.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81"})]})})}}}]);
+//# sourceMappingURL=859.700240b6.chunk.js.map
